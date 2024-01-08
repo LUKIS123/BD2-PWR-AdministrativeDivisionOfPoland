ec9a19e009960820f4dd537879221bf91ebbfd2f
Czerworopucha <59446206+Czerworopucha@users.noreply.github.com>
2024-01-08 20:35:11 +0100

    reposy chyba gotowe

---
+++ AdministrativeDivisionOfPoland-Backend/API/src/main/java/pl/edu/pwr/administrativedivisionofpolandbackend/Repositories/CountyRegisteredOfficeRepository.java
@@ -0,0 +1,51 @@
+package pl.edu.pwr.administrativedivisionofpolandbackend.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import pl.edu.pwr.administrativedivisionofpolandbackend.Entities.CountyRegisteredOffice;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CountyRegisteredOfficeRepository extends JpaRepository<CountyRegisteredOffice, Integer> {
+
+    Optional<CountyRegisteredOffice> findById(int id);
+
+    @Query(nativeQuery = true, value = """
+            select * from siedziby_powiatow\s
+            where id_pow = ?1\s
+            order by miejscowosc_siedziby\s
+            """)
+    List<CountyRegisteredOffice> getCountyOfficeByCountyId(Integer countyId);
+
+    @Query(nativeQuery = true, value = """
+            select * from siedziby_powiatow\s
+            order by miejscowosc_siedziby\s
+            offset ?1 rows\s
+            fetch first ?2 row only\s
+            """)
+    List<CountyRegisteredOffice> getAllCountyOffice(Integer offsetRows, Integer fetchRows);
+
+    @Query(nativeQuery = true, value = """
+            select count(*) from siedziby_powiatow\s
+            """)
+    Integer getCountyOfficeCount();
+
+    @Query(nativeQuery = true, value = """
+            select * from siedziby_powiatow\s
+            where lower(siedziby_powiatow.miejscowosc_siedziby) like(?1)\s
+            order by miejscowosc_siedziby\s
+            offset ?2 rows\s
+            fetch first ?3 row only\s
+            """)
+    List<CountyRegisteredOffice> searchCountyOfficeByName(String searchPhrase, Integer offsetRows, Integer fetchRows);
+
+    @Query(nativeQuery = true, value = """
+            select count(*) from siedziby_powiatow\s
+            where lower(siedziby_powiatow.miejscowosc_siedziby) like(?1)\s
+            """)
+    Integer getCountFromSearchCountyOffice(String searchPhrase);
+}
---
+++ AdministrativeDivisionOfPoland-Backend/API/src/main/java/pl/edu/pwr/administrativedivisionofpolandbackend/Repositories/RegisteredOfficeAddressesRepository.java
@@ -0,0 +1,39 @@
+package pl.edu.pwr.administrativedivisionofpolandbackend.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import pl.edu.pwr.administrativedivisionofpolandbackend.Entities.RegisteredOfficeAddresses;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface RegisteredOfficeAddressesRepository extends JpaRepository<RegisteredOfficeAddresses, Integer> {
+
+    Optional<RegisteredOfficeAddresses> findById(int id);
+
+    @Query(nativeQuery = true, value = """
+            select * from adresy_siedzib ad\s
+            inner join siedziby_wojewodztw sw on ad.id_adresu_siedziby = sw.id_adresu_siedziby\s
+            where sw.id_woj = ?1\s
+            order by miejscowosc\s
+            """)
+    List<RegisteredOfficeAddresses> getRegisteredOfficeAddressByVoivodeshipId(Integer voivodeshipId);
+
+    @Query(nativeQuery = true, value = """
+            select * from adresy_siedzib ad\s
+            inner join siedziby_powiatow sp on ad.id_adresu_siedziby = sp.id_adresu_siedziby\s
+            where sp.id_pow = ?1\s
+            order by miejscowosc\s
+            """)
+    List<RegisteredOfficeAddresses> getRegisteredOfficeAddressByCountyId(Integer countyId);
+
+    @Query(nativeQuery = true, value = """
+            select * from adresy_siedzib ad\s
+            inner join siedziby_gmin sg on ad.id_adresu_siedziby = sg.id_adresu_siedziby\s
+            where sg.id_woj = ?1\s
+            order by miejscowosc\s
+            """)
+    List<RegisteredOfficeAddresses> getRegisteredOfficeAddressByCommuneId(Integer communeId);
+}
